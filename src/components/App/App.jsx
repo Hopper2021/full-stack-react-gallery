50fdd59ec1502953091ad60dee514d86509a6735
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-16 21:31:37 -0500

    Added map of gallerylist and base gallery divs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,19 +7,30 @@ import galleryItems from '../../../server/modules/gallery.data';
 function App() {
   axios({
     method: 'GET',
-    url: '/'
+    url: '/gallery'
   }).then((response) => {
     setGallery(response.data);
   }).catch((error) => {
     alert('Something went wrong!');
     console.log('Error in GET,', error);
   })
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
+        {galleryItems.map(item => (
+          <div id="gallery-container">
+            <div key={item.id}>
+              <div>Image:{item.path}</div>
+              <div>Description{item.description}</div>
+              <button>love it!</button>
+              <div>Likes:{item.likes}</div>
+            </div>
+          </div>
+        ))}
         <img src="images/goat_small.jpg"/>
       </div>
     );
