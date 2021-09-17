a34ad0b44836f6b6a8654274212965fb1298af98
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-16 21:19:41 -0500

    imported axios, useSTate, useEffect and added client GET

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,19 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
+import axios from 'axios';
 import './App.css';
+import galleryItems from '../../../server/modules/gallery.data';
 
 function App() {
+  axios({
+    method: 'GET',
+    url: '/'
+  }).then((response) => {
+    setGallery(response.data);
+  }).catch((error) => {
+    alert('Something went wrong!');
+    console.log('Error in GET,', error);
+  })
     return (
       <div className="App">
         <header className="App-header">
