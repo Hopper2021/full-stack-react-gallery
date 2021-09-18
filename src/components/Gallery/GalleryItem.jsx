0410f9441304d04f6bb6b8581cdf86c302d4d909
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-18 09:18:31 -0500

    Added useState const and called conditional render function on image div

--- src/components/Gallery/GalleryItem.jsx
+++ src/components/Gallery/GalleryItem.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 function GalleryItem(props) {
 
+    const [imageDisplayed, setImageDisplayed] = useState(true);
+
     // If image is displayed change image to item description
     // if item description is displayed change description back to image
     const displayDescriptionOrImage = () => {
@@ -19,7 +21,7 @@ function GalleryItem(props) {
                 when image div is clicked */}
             {props.list.map(item => (
                 <div>
-                    <div key={item.id} onClick={() => displayDescriptionOrImage(item.description)}>
+                    <div key={item.id} onClick={displayDescriptionOrImage}>
                         <img src={item.path}></img>
                     </div>
                     <div className="item-description">{item.description}</div>
