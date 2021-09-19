d8c32a9ca05aeceeea9ccfbf0518dbde14938b94
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-19 15:51:36 -0500

    Added GalleryItems code comments

--- src/components/Gallery/GalleryItems.jsx
+++ src/components/Gallery/GalleryItems.jsx
@@ -1,10 +1,14 @@
 import GalleryItem from '../Gallery/GalleryItem';
 
+// Function that is passed likeItem and list through to parent
 function GalleryItems({list, likeItem}) {
     return(
         <div>
             {/* Loop through gallery */}
             {list.map(item => (
+                // Define a unique id to help React render items faster
+                // and define likeItem to be further passed up to App.jsx
+                // define item for each gallery item rendered
                 <GalleryItem key={item.id} item={item} likeItem={likeItem}/>
             ))}
         </div>
